3007dbb548027932ab6e5fa3b10fa4d313f3804c
Adnan Memic <adnan.memic27@gmail.com>
2025-04-15 23:55:25 +0200

    Add glazewm and windows-terminal configs to script

--- back-config-skript.ps1
+++ back-config-skript.ps1
@@ -1,8 +1,18 @@
 # destination
 $target = "$HOME\configFiles"
 
+# windows-terminal files
+Copy-Item "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "$target\windows-terminal" -Force
+Copy-Item $PROFILE "$target\windows-terminal" -Force
+
+
 # vscode files
-Copy-Item "$env:APPDATA\Code\User\settings.json" $target\vscode -Force
-Copy-Item "$env:APPDATA\Code\User\keybindings.json" $target\vscode -Force
+Copy-Item "$env:APPDATA\Code\User\settings.json" "$target\vscode" -Force
+Copy-Item "$env:APPDATA\Code\User\keybindings.json" "$target\vscode" -Force
+
+# glazewm files
+Copy-Item "$HOME\.glzr\glazewm\config.yaml" "$target\glazewm" -Force
+
+
 
 Write-Host "=====All files were succesfully copied to $target====="
